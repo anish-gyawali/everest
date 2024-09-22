a766e8fb06a35dc63e20964b577ddf2f95de0111
Anish Gyawali <anish.gyawali7@gmail.com>
2024-09-21 22:15:37 -0500

    formatted code

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className="bg-black text-right font-sans text-white p-3">
+        <p>{"© 2024 September Anish Gyawali staight out from Hetauda"}</p>
+      </footer>
+    )
+}
